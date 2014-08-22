31c47897630ad26df5de9eb23f55c39e951a86b4
Patrick Laurin <mosieurlaurin@gmail.com>
2014-08-22 14:51:32 -0400

    Removed interruptions
    On a enlevé les interruptions, HTTP fonctionne bien.

--- box/box.ino
+++ box/box.ino
@@ -60,20 +60,20 @@ void setup()
   }  
 
   // Configuration des interruptions
-  cli(); //stop interrupts
+  //cli(); //stop interrupts
   //set timer1 interrupt at 1Hz
-  TCCR1A = 0;// set entire TCCR1A register to 0
-  TCCR1B = 0;// same for TCCR1B
-  TCNT1  = 0;//initialize counter value to 0
+  //TCCR1A = 0;// set entire TCCR1A register to 0
+  //TCCR1B = 0;// same for TCCR1B
+  //TCNT1  = 0;//initialize counter value to 0
   // set compare match register for 1hz increments
-  OCR1A = 15624;// = (16*10^6) / (1*1024) - 1 (must be <65536)
+  //OCR1A = 15624;// = (16*10^6) / (1*1024) - 1 (must be <65536)
   // turn on CTC mode
-  TCCR1B |= (1 << WGM12);
+  //TCCR1B |= (1 << WGM12);
   // Set CS10 and CS12 bits for 1024 prescaler
-  TCCR1B |= (1 << CS12) | (1 << CS10);  
+  //TCCR1B |= (1 << CS12) | (1 << CS10);  
   // enable timer compare interrupt
-  TIMSK1 |= (1 << OCIE1A);  
-  sei();//allow interrupts
+  //TIMSK1 |= (1 << OCIE1A);  
+  //sei();//allow interrupts
 }
 
 //------------------------//
@@ -99,6 +99,10 @@ ISR(TIMER1_COMPA_vect){
 
 void event()
 {
+  Serial.println("Event!");
+  delay(500);
+     
+  Serial.println("Température:");
   String PostData="{\"path\":\"netspresso.relay.01\",\"value\":3,\"units\":\"watts\",\"epoch\":\"2014-08-09T05:46:06-0400\"}";
   int temp;
   temp = thermocouple.measure(TEMPC);
@@ -113,6 +117,23 @@ void event()
   client.print("Content-Length: ");
   client.println(PostData.length());
   client.println();   
+  client.println(PostData);
+  
+  delay(1000);
+  Serial.println("delay");
+  // if there are incoming bytes available
+  // from the server, read them and print them:
+  //boolean waiting=true;
+  //while (waiting)
+  //{
+  Serial.print("Réponse baby:");
+  while(client.available())
+  {
+    char c = client.read();
+    Serial.print(c);
+  }
+  //}
+  Serial.println("");
 }
 
 //--------------------//
@@ -121,42 +142,57 @@ void event()
 
 void loop()
 {
+  delay(5000);
   do
   {
-    if (flag)
+    if (true)
+    //if (flag)
     {
-      cli();
+      //cli();
       flag = 0;
-      if (client.connect(server, 80))
+      // if the server's disconnected, stop the client:
+      if (!client.connected())
       {
-        Serial.println("--> Connecté");
-        event();
-      }
-      else
-      {
-        // Si la connection au serveurs est impossible:
-        Serial.println("--> Connection impossible!");
-      }
-      sei();
+        Serial.println();
+        Serial.println("--> Déconnecté...");
+        Serial.println("");
+        client.stop();
+        
+        if (client.connect(server, 80))
+        {
+          Serial.println("--> Connecté");
+        }
+        else
+        {
+          // Si la connection au serveurs est impossible:
+          Serial.println("--> Connection impossible!");
+          //return;
+        }  
+      } //if (!client.connected())
+      event();
+      //sei();
     }
 
-    // if there are incoming bytes available
-    // from the server, read them and print them:
-/*    if (client.available())
-    {
-      char c = client.read();
-      Serial.print(c);
-    }
-  
-    // if the server's disconnected, stop the client:
+/*    // if the server's disconnected, stop the client:
     if (!client.connected())
     {
       Serial.println();
       Serial.println("--> Déconnecté...");
       Serial.println("");
       client.stop();
-    }
-*/ 
+      
+      if (client.connect(server, 80))
+      {
+        Serial.println("--> Connecté");
+      }
+      else
+      {
+        // Si la connection au serveurs est impossible:
+        Serial.println("--> Connection impossible!");
+        return;
+      }  
+    } //if (!client.connected()) */
+    
     delay(200);
   } while(true);  
 }
